bbcbfbea87314cb183b245001aa28b14d2ad1168
Seth Randell <sethhrandell@gmail.com>
2019-08-28 12:00:26 -0400

    Addeded html and api routed and got the form working to add data to the array

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+  var friends = [
+	{
+		name: "Ahmed",
+		photo: "data:https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvDt0nGM4wCOXAsWVYNi_mhIgt0Kx4OhRONViibpDrpmgqfLlUyg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    
+  ]
+
+  module.exports = friends;
+
