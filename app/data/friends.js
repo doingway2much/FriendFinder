3fe9d7d30f6439f2e6939390808ba70d942652b0
Seth Randell <sethhrandell@gmail.com>
2019-08-29 00:08:01 -0400

    Tighened up UI and really added a theme

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
   var friends = [
 	{
 		"name": "Richard Hendricks",
-		"photo": "data:https://www.magicalquote.com/wp-content/uploads/2015/07/Richard-Hendricks.jpg",
+		"photo": "https://www.magicalquote.com/wp-content/uploads/2015/07/Richard-Hendricks.jpg",
 		"scores": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     },
     {
       "name": "Nelson 'Big Head' Bighetti",
-      "photo": "data:http://siliconvalleyism.com/characters/big-head-small.jpg",
+      "photo": "http://siliconvalleyism.com/characters/big-head-small.jpg",
       "scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
       }  
   ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,35 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
-  // Return all friends found in friends.js as JSON
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     console.log(req.body);
-
     var coder = req.body;
-    
-    
-
-    // compute best match from scores
     var coderMatch = {};
 
-    for(var i = 0; i < coder.scores.length; i++) {
-      if(coder.scores[i] == "1 (I would never say that!!!!)") {
+    for (var i = 0; i < coder.scores.length; i++) {
+      if (coder.scores[i] == "1 (I would never say that!!!!)") {
         coder.scores[i] = 1;
-      } else if(coder.scores[i] == "5 (Damm are you reading my mind)") {
+      } else if (coder.scores[i] == "5 (Damm are you reading my mind)") {
         coder.scores[i] = 5;
       } else {
         coder.scores[i] = parseInt(coder.scores[i]);
       }
     }
 
-
     var coderMatchIndex = 0;
-
     var coderMatchDif = 40;
 
-    for(var i = 0; i < friends.length; i++) {
+    for (var i = 0; i < friends.length; i++) {
       var totalDif = 0;
 
-      for(var index = 0; index < friends[i].scores.length; index++) {
+      for (var index = 0; index < friends[i].scores.length; index++) {
         var DifOneScore = Math.abs(friends[i].scores[index] - coder.scores[index]);
         totalDif += DifOneScore;
       }
-
 
       if (totalDif < coderMatchDif) {
         coderMatchIndex = i;
@@ -50,6 +41,6 @@ module.exports = function(app) {
     coderMatch = friends[coderMatchIndex];
     friends.push(coder);
     res.json(coderMatch);
-    });
+  });
 
 };
